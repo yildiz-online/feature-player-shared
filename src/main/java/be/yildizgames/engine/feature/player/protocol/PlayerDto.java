d598f903c9b00605fd8821f80fea8f5d86f2f93e
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2017-11-24 19:29:37 +0100

    [YFPS-0] Initial commit.

---
+++ src/main/java/be/yildizgames/engine/feature/player/protocol/PlayerDto.java
@@ -0,0 +1,78 @@
+/*
+ * This file is part of the Yildiz-Engine project, licenced under the MIT License  (MIT)
+ *
+ *  Copyright (c) 2017 Grégory Van den Borre
+ *
+ *  More infos available: https://www.yildiz-games.be
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ *  documentation files (the "Software"), to deal in the Software without restriction, including without
+ *  limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ *  of the Software, and to permit persons to whom the Software is furnished to do so,
+ *  subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all copies or substantial
+ *  portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ *  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ *  OR COPYRIGHT  HOLDERS BE LIABLE FOR ANY CLAIM,
+ *  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  SOFTWARE.
+ *
+ */
+
+package be.yildizgames.engine.feature.player.protocol;
+
+import be.yildiz.common.id.PlayerId;
+import be.yildizgames.engine.feature.player.PlayerStatus;
+
+/**
+ * @author Grégory Van den Borre
+ */
+public class PlayerDto {
+
+    /**
+     * Player unique Id.
+     */
+    public final PlayerId player;
+
+    /**
+     * Player name.
+     */
+    public final String login;
+
+    /**
+     * Player status for the current player.
+     */
+    public final PlayerStatus status;
+
+    public PlayerDto(PlayerId player, String login, PlayerStatus status) {
+        super();
+        this.player = player;
+        this.login = login;
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        PlayerDto playerDto = (PlayerDto) o;
+
+        return player.equals(playerDto.player) && login.equals(playerDto.login) && status == playerDto.status;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = player.hashCode();
+        result = 31 * result + login.hashCode();
+        result = 31 * result + status.hashCode();
+        return result;
+    }
+}
